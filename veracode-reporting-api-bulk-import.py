cf45007600eb1c6690cbfd1ba8128f849f248dec
rpereira <ricardoapjunior@hotmail.com>
2024-05-28 16:57:58 +0100

    Adding support for pagination

--- veracode-reporting-api-bulk-import.py
+++ veracode-reporting-api-bulk-import.py
@@ -47,11 +47,11 @@ def request_report(json_data):
     else:
         response.raise_for_status()
 
-def get_report_data(report_id):
+def get_report_data(report_id, page):
     global api_base
     global headers
     global auth
-    report_status_endpoint = f"{api_base}/appsec/v1/analytics/report/{report_id}"
+    report_status_endpoint = f"{api_base}/appsec/v1/analytics/report/{report_id}?page={page}"
     response = requests.get(report_status_endpoint, auth=auth, headers=headers)
 
     if response.ok:
@@ -177,26 +177,48 @@ def parse_flaw_list(flaw_list, is_application_data):
         del flaw["Application"]
 
     return flaw_list
+
+def get_findings_for_all_pages(report_id, embedded_node):
+    findings = embedded_node["findings"]
+    if not "page_metadata" in embedded_node:
+        return findings
+    page_metadata = embedded_node["page_metadata"]
+    current_page = page_metadata["number"]
+    max_page = page_metadata["total_pages"]
+    if max_page <= 1:
+        return findings
     
+    print(f"Parsing additional pages for report {report_id}")
+    current_page = current_page + 1
+
+    while current_page < max_page:
+        print(f"Parsing page {current_page}/{max_page}")
+        next_page = get_report_data(report_id, current_page)
+        if next_page and "_embedded" in next_page and "findings" in next_page["_embedded"]:
+            findings = findings + next_page["_embedded"]["findings"]
+        current_page = current_page + 1
+
+    return findings
+
 
 def get_report_results(report_id, current_start_date, end_date, directory, is_application_data):
     for status_attempt in range(1, max_poll_attempts + 1):
         print(f"Checking Veracode report status for date range {current_start_date}-{end_date}. Attempt {status_attempt}/{max_poll_attempts}...")
-        report_data = get_report_data(report_id)
+        report_data = get_report_data(report_id, 0)
 
         if report_data is None:
-            print(f"Error generating report for date range {current_start_date}-{end_date}. Skipping it")
+            print(f"ERROR: empty report for date range {current_start_date}-{end_date}. Skipping it")
             return
         status = report_data['_embedded']['status']
         if status == "COMPLETED":
-            print("Veracode report completed. Saving it to a file...")
+            print("SUCCESS: report fetched successfully. Saving it to a file...")
             output_file = f'veracode_data_dump {current_start_date.strftime("%Y-%m-%d")} to {end_date.strftime("%Y-%m-%d")}.csv'
-            save_report_to_csv(os.path.join(directory, output_file), parse_flaw_list(report_data['_embedded']['findings'], is_application_data))
+            save_report_to_csv(os.path.join(directory, output_file), parse_flaw_list(get_findings_for_all_pages(report_id, report_data['_embedded']), is_application_data))
             return
         elif status == "PROCESSING":
             time.sleep(poll_interval_seconds)
         else:
-            print(f"Unexpected report status: {status} found for date range {current_start_date}-{end_date}. Skipping it")
+            print(f"ERROR: unexpected report status {status} found for date range {current_start_date}-{end_date}. Skipping it")
             return
     print (f"Report timed out after {max_poll_attempts*poll_interval_seconds} seconds for range {current_start_date}-{end_date}. Try it later with id: {report_id}")
             
